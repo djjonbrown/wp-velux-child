82edf2f76659cdf3731b8f0ebd75e074579dbae0
jonathan-at-prosody <jonathan.brown@prosody.co>
2018-09-23 16:38:54 -0400

    upated links to template and Theme URI

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'parent-style'; 
+    $parent_style = 'wp-velux-theme'; 
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
